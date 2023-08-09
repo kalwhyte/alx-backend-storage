a60d0e1a9d6c9263aca200a71f4da47348bdf9be
whytewalker <whytewalkerinstalltion@gmail.com>
2023-08-09 20:43:35 +0100

    old school band

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+
+SELECT band_name, lifespan FROM metal_bands
+WHERE main_style = 'Glam rock'
+ORDER BY lifespan DESC
