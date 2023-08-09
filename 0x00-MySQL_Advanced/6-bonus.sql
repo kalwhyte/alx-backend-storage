6a2f19364ad240ac909155b6f79f6d30c60c85c7
whytewalker <whytewalkerinstalltion@gmail.com>
2023-08-10 00:50:10 +0100

    Adding a new correction

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,28 +1,29 @@
--- Write a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
--- Procedure AddBonus is taking 3 inputs (in this order)
--- user_id, a users.id value (you can assume user_id is linked to an existing users)
--- project_name, a new or already exists projects - if no projects.name found in the table, you should create it
--- score, the score value of this correction
+-- SQL script that creates a stored procedure
+-- that adds a bonus to a user for a project
 DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER $$
 CREATE PROCEDURE AddBonus(user_id INT, project_name VARCHAR(255), score FLOAT)
 BEGIN
-    DECLARE project_id INT;
+    DECLARE project_id INT DEFAULT 0;
+    DECLARE project_count INT DEFAULT 0;
 
-    SELECT id INTO project_id
-    FROM projects
-    WHERE name = project_name;
-
-    IF project_id IS NULL THEN
+    SELECT COUNT(*) 
+        INTO project_count
+        FROM projects
+        WHERE name = project_name;
+    
+    IF project_count = 0 THEN
         INSERT INTO projects (name)
         VALUES (project_name);
-
         SET project_id = LAST_INSERT_ID();
+    ELSE
+        SELECT id
+            INTO project_id
+            FROM projects
+            WHERE name = project_name;
     END IF;
 
     INSERT INTO corrections (user_id, project_id, score)
-    VALUES (p-user_id, project_id, p_score);
-
-    SELECT CONCAT('Correction added for user ', user_id, ' in project ', project_name, ' with score', score) AS message;
-END $$
+        VALUES (user_id, project_id, score);
+END$$
 DELIMITER ;
