3d1b7a18d21313bedbc6d8d123a24c9fededecf2
whytewalker <whytewalkerinstalltion@gmail.com>
2023-08-10 02:07:08 +0100

    SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,13 +1,13 @@
 -- SQL script that creates a stored procedure ComputeAverageScoreForUser
 --that computes and store the average score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score FLOAT;
-    DECLARE total_score FLOAT;
-    DECLARE total_projects INT;
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
 
-    SELECT SUM(score), COUNT(*) INTO total_score, total_projects
+    SELECT SUM(score), COUNT(*) INTO total_score, projects_count
     FROM corrections
     WHERE user_id = user_id;
 
