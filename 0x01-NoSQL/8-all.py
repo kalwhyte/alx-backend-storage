23bff67bada1f0690da7edf65fc841042760c7a3
kalwhyte <superkalel55@gmail.com>
2023-08-14 16:34:06 -0700

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,11 @@
 #!/urs/bin/env python3
 '''lists all documents in a collection
 '''
+from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
-    '''Return an empty list
+    '''Return the list
     '''
-    return [doc for doc in mongo_collection.find()]
+    doc_list = list(doc)
+    return doc_list
