d1def2faede44ad08c5eff9db98a7ba922c02901
kalwhyte <superkalel55@gmail.com>
2023-08-15 15:39:53 -0700

    101-students.py

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+''' 101-student.py '''
+
+
+from pymongo import MongoCLient
+
+
+def top_students(mong_collection):
+    ''' Print all students in collection sorted by average score
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
